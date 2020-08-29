35735c861193d280775fe4acee169237d8526966
Monty Galloway <monty.j.galloway@gmail.com>
2020-08-29 17:10:43 +1000

    Add testresponse macro for view data

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,25 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // allows us to use $response->data($key) to check
+        // data was passed to views
+        TestResponse::macro('data', function ($key) {
+            $data = $this->original->getData();
+            if (!isset($data[$key])) {
+                throw new \Exception("Key \"$key\" not found in response data");
+            }
+
+            return $data[$key]->all();
+        });
+    }
 }
