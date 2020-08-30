f2b6682a4deb7830579f5fba8d9a53cc622029ed
Monty Galloway <monty.j.galloway@gmail.com>
2020-08-31 00:29:28 +1000

    Some graph styling

--- resources/js/components/SalesGraph/SalesGraph.vue
+++ resources/js/components/SalesGraph/SalesGraph.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="graph">
-        <line-chart v-bind:chart-data="chartData"></line-chart>
+        <line-chart
+            v-bind:styles="styles"
+            v-bind:options="options"
+            v-bind:chart-data="chartData"></line-chart>
     </div>
 </template>
 <script>
 import LineChart from "./LineChart";
 
 export default {
+    data() {
+        return {
+            styles: {
+                height: "100%",
+                paddingLeft: "0",
+                marginLeft: "0",
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+            }
+        }
+    },
     components: {
         'line-chart': LineChart,
     },
@@ -16,13 +32,15 @@ export default {
             const dates = [];
             const totals = [];
             Object.keys(byDay).forEach(key => {
-                dates.push(key);
+                const dateTokes = key.split('-');
+                dates.push(`${dateTokes[2]}/${dateTokes[1]}`);
                 totals.push(byDay[key]);
             });
             return {
                 labels: dates,
                 datasets: [{
                     data: totals,
+                    label: "Sales"
                 }]
             };
         }
