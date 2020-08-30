44bb20dd9c524acf257aa5402f60fc6b685e895b
Monty Galloway <monty.j.galloway@gmail.com>
2020-08-30 11:23:42 +1000

    Add data tests to dashboard view/controller

--- app/Http/Controllers/SalesDashboardController.php
+++ app/Http/Controllers/SalesDashboardController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Sales;
+use App\SalesStats;
+use Carbon\Carbon;
+
 class SalesDashboardController extends Controller
 {
     public function show()
     {
-        return view('dashboard.show');
+        $to = Carbon::now();
+        $from = Carbon::now()->subMonth();
+
+        $sales = Sales::byDateRange($from, $to)->orderBy('date', 'asc')->get();
+
+        $totalsByDay = SalesStats::totalsByDay($sales);
+        return view('dashboard.show', [
+            'totals_by_day' => $totalsByDay,
+            'from_date' => $from->format('Y-m-d'),
+            'to_date' => $to->format('Y-m-d'),
+        ]);
     }
 }
--- tests/Feature/SalesDashboardTest.php
+++ tests/Feature/SalesDashboardTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Sales;
+use App\SalesStats;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class SalesDashboardTest extends TestCase
@@ -10,8 +13,44 @@ class SalesDashboardTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
+        $to = Carbon::now();
+        $from = Carbon::now()->subMonth();
+
+        factory(Sales::class)->createMany(
+            [
+                [
+                    'date' => $to->copy()->subDay(),
+                ],
+                [
+                    'date' => $to->copy()->subDay(2),
+                ],
+                [
+                    'date' => $to->copy()->subDay(3),
+                ],
+                [
+                    // one out of range
+                    'date' => $to->copy()->subMonth(3),
+                ],
+            ]
+        );
+
+        $sales = Sales::byDateRange($from, $to)->get();
+
+        $totalsByDay = SalesStats::totalsByDay($sales);
+
         $response = $this->get('/dashboard');
         $response->assertSuccessful();
         $response->assertViewIs('dashboard.show');
+        $injectedDailyStats = $response->data('totals_by_day');
+
+        $this->assertCount(3, $injectedDailyStats);
+        $this->assertCount(count($totalsByDay), $injectedDailyStats);
+
+        $this->assertEquals($from->format('Y-m-d'), $response->data('from_date'));
+        $this->assertEquals($to->format('Y-m-d'), $response->data('to_date'));
+
+        foreach ($injectedDailyStats as $date => $count) {
+            $this->assertEquals($totalsByDay[$date], $count);
+        }
     }
 }
